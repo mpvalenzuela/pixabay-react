6e498cead0fb80ba1f9a2c70166e09a38f3b0c22
mpvalenzuela <mpvalenzuela710@gmail.com>
2022-08-17 14:18:14 -0400

    version 1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useState} from 'react';
+import Formulario from './components/Formulario';
+import ListadoImagenes from './components/ListadoImagenes';
 
 function App() {
+
+const [ busqueda, guardarBusqueda ] = useState('');
+const [ imagenes, guardarImagenes ] = useState([]);
+
+useEffect(() => {
+ const consultarApi = async () => {
+  if(busqueda === '' ) return;
+
+  const imagenesPorPagina = 30;
+  const key = '28940066-ea2f8e5144370873bc71dd2a4';
+  const url = `https://pixabay.com/api/?key=${key}&q=${busqueda}&per_page=${imagenesPorPagina}`;
+
+  const respuesta = await fetch(url);
+  const resultado = await respuesta.json();
+
+  guardarImagenes(resultado.hits);
+ }
+consultarApi();
+}, [busqueda])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <div className='jumbotron'>
+      <p className='lead text-center'>Buscador de imagenes</
+      p>
+
+      <Formulario 
+      guardarBusqueda = {guardarBusqueda}
+      />
+      </div>
+
+      <div className='row justify-content-center'>
+      <ListadoImagenes 
+      imagenes={imagenes}
+      />
+      </div>
+
     </div>
   );
 }
